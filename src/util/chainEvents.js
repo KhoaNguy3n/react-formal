42b5aa91feb322ef2703a72fbb264809d8e9c753
KhoaNguyen <khoanguyendnvn@gmail.com>
2016-09-26 13:20:44 +0700

    Update import and dependencies

--- src/util/chainEvents.js
+++ src/util/chainEvents.js
@@ -1,4 +1,4 @@
-import has from 'lodash/has';
+import { has } from 'lodash/has';
 import chain from 'chain-function';
 
 function chainEvents(events, objects) {
@@ -10,7 +10,7 @@ function chainEvents(events, objects) {
     let handlers = objects.map(p => p[event]);
     result[event] = chain(...handlers)
   })
-  
+
   return result
 }
 
